041ec67ca84de1eab914f81f21d865ab859b57e9
unknown <jrwilson5116@gmail.com>
2018-02-19 22:30:57 -0600

    added welcome screen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-import input_checks
+from input_checks import valid_email, valid_length, match
 
 app = Flask(__name__)
 app.config['DEBUG']=True
@@ -7,8 +7,19 @@ app.config['DEBUG']=True
 
 @app.route("/")
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
+@app.route("/",methods=['POST'])
+def process():
+    username = request.form['username']
+    password_1 = request.form['password']
+    password_2 = request.form['verify']
+    email = request.form['email']
+
+    if valid_length(username):
+        return render_template('welcome.html')
+    else:
+        return render_template('index.html')
 
 
 if __name__=="__main__":
